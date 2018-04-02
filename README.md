4e5df9faffa32fe95c350f11598b8ededa371710
Brian <brian9206@hotmail.com>
2018-04-03 00:48:40 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Yahoo! Financial Historical Data Fetcher
+A simple Node.JS script that helps you to fetch financial historical data from Yahoo! Finance.
+
+## Usage
+`node fetcher <stock symbol> <start period (unix timestamp)> <end period (unix timestamp)>`
---
+++ fetcher.js
@@ -0,0 +1,77 @@
+const request = require('request');
+const jar = request.jar();
+
+function print_help() {
+    console.log('Usage: node fetcher <stock symbol> <start period (unix timestamp)> <end period (unix timestamp)>');
+}
+
+// check arguments
+if (process.argv.length < 5) {
+    print_help();
+    return;
+}
+
+const symbol = process.argv[2];
+const period1 = parseInt(process.argv[3]);
+const period2 = parseInt(process.argv[4]);
+
+if (isNaN(period1) || isNaN(period2)) {
+    print_help();
+    return;
+}
+
+request(`https://finance.yahoo.com/quote/${symbol}/history`, { jar }, (error, response, body) => {
+    if (error) {
+        console.error('Failed to fetch \'crumb\' from Yahoo! server:', error.message);
+        process.exit(128);
+        return;
+    }
+    
+    // search for crumb.
+    // FIXME: regex is seems incorrect
+    const match = body.match(/"CrumbStore":{"crumb":"([0-9A-Za-z.-]+)"}/);
+    
+    if (!match || match.length < 2) {
+        console.error('Yahoo! server does not respond with a correct format of HTML.', body);
+        process.exit(128);
+        return;
+    }
+
+    // download the CSV
+    request(`https://query1.finance.yahoo.com/v7/finance/download/${symbol}`, {
+        qs: {
+            period1,
+            period2,
+            interval: '1d',
+            events: 'history',
+            crumb: match[1]
+        },
+        jar
+    }, (error, response, body) => {
+        if (error) {
+            console.error('Failed to fetch CSV from Yahoo! server:', error.message);
+            return;
+        }
+        
+        // convert to matlab readable format
+        const lines = body.split(/\r?\n/g).splice(1);
+        
+        for (const line of lines) {
+            // Date,	Open,	High,	Low	Close,	Adj Close,	Volume
+            const data = line.split(',');
+            
+            const date = new Date(data[0]);
+            
+            if (isNaN(date.getTime())) {
+                continue;
+            }
+            
+            data[0] = date.getFullYear() + ',' + (date.getMonth() + 1) + ',' + date.getDate();
+            
+            // use stdout to keep data unformatted
+            process.stdout.write(data.join(',') + '\n');
+        }
+        
+        process.exit(0);
+    });
+});
